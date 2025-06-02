4f9c63ae21f592a01f720f77cd1d21868b2389f0
abhi-IC <abhishekp@innovocommerce.com>
2025-06-02 11:02:07 -0700

    using the SHA1 password hasher

--- src/MembershipIdentityProvider/Code/Identity/MembershipPasswordHasher.cs
+++ src/MembershipIdentityProvider/Code/Identity/MembershipPasswordHasher.cs
@@ -4,49 +4,48 @@ using System.Text;
 
 namespace MembershipIdentityProvider.Code.Identity
 {
-	public class MembershipPasswordHasher<TUser> : IPasswordHasher<TUser>
-		where TUser : MembershipUser
-	{
+	//public class MembershipPasswordHasher<TUser> : IPasswordHasher<TUser>
+	//	where TUser : MembershipUser
+	//{
 
-		public string HashPassword(TUser user, string password)
-		{
-			var saltBytes = Convert.FromBase64String(user.PasswordSalt);
-			var rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, saltBytes, 10000, HashAlgorithmName.SHA256);
-			var hashPassword = Convert.ToBase64String(rfc2898DeriveBytes.GetBytes(63));
+	//	public string HashPassword(TUser user, string password)
+	//	{
+	//		var saltBytes = Convert.FromBase64String(user.PasswordSalt);
+	//		var rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, saltBytes, 10000, HashAlgorithmName.SHA256);
+	//		var hashPassword = Convert.ToBase64String(rfc2898DeriveBytes.GetBytes(63));
 
-			return hashPassword;
-		}
+	//		return hashPassword;
+	//	}
 
-		public PasswordVerificationResult VerifyHashedPassword(TUser user, string hashedPassword, string providedPassword)
-			=> user.PasswordFormat 
-			switch
-			{
-				0 => hashedPassword.Equals(providedPassword) ? PasswordVerificationResult.Success : PasswordVerificationResult.Failed,
-				1 => hashedPassword.Equals(HashPassword(user, providedPassword)) ? PasswordVerificationResult.Success : PasswordVerificationResult.Failed,
-				_ => PasswordVerificationResult.Failed
-			};
+	//	public PasswordVerificationResult VerifyHashedPassword(TUser user, string hashedPassword, string providedPassword)
+	//		=> user.PasswordFormat 
+	//		switch
+	//		{
+	//			0 => hashedPassword.Equals(providedPassword) ? PasswordVerificationResult.Success : PasswordVerificationResult.Failed,
+	//			1 => hashedPassword.Equals(HashPassword(user, providedPassword)) ? PasswordVerificationResult.Success : PasswordVerificationResult.Failed,
+	//			_ => PasswordVerificationResult.Failed
+	//		};
 
-		public static string GenerateSalt()
-		{
-			byte[] buf = new byte[16];
-			new RNGCryptoServiceProvider().GetBytes(buf);
+	//	public static string GenerateSalt()
+	//	{
+	//		byte[] buf = new byte[16];
+	//		new RNGCryptoServiceProvider().GetBytes(buf);
 
-			return Convert.ToBase64String(buf);
-		}
+	//		return Convert.ToBase64String(buf);
+	//	}
 
-		public static string GetPassword(TUser user, int passwordFormat, string password)
-		{
-			return passwordFormat switch
-			{
-				0 => password,
-				1 => new MembershipPasswordHasher<TUser>().HashPassword(user, password),
-				_ => password
-			};
-		}
-    }
+	//	public static string GetPassword(TUser user, int passwordFormat, string password)
+	//	{
+	//		return passwordFormat switch
+	//		{
+	//			0 => password,
+	//			1 => new MembershipPasswordHasher<TUser>().HashPassword(user, password),
+	//			_ => password
+	//		};
+	//	}
+    //}
 
-    /* --This may be more appropriate. Needs To be Tested---
-     
+    /* --This may be more appropriate. Needs To be Tested---*/     
     public class MembershipPasswordHasher<TUser> : IPasswordHasher<TUser>
         where TUser : MembershipUser
     {
@@ -100,6 +99,6 @@ namespace MembershipIdentityProvider.Code.Identity
         }
     }
     
-    */
+    /*--*/
 
 }
